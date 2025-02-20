67c647b25675ad0e440a728021d2c22b94b3603e
Frog1810 <frogblaauw@gmail.com>
2025-02-20 14:20:22 +0100

    Update files

--- Code/index.php
+++ Code/index.php
@@ -3,12 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+          content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="main.css">
     <title>Space Clicker</title>
 </head>
 <body>
+<div class="titleBar">
+    <h1 class="title">Planet Clicker</h1>
+</div>
+<div class="middle">
 
+</div>
 </body>
 </html>
