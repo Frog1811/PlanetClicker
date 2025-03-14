6f476e5ca46b7a1fdf5914b52f958fc9410200b5
Frog1810 <frogblaauw@gmail.com>
2025-03-14 08:59:46 +0100

    Start Shop

---
+++ Code/Shop/Background.js
@@ -0,0 +1,18 @@
+class background {
+    color;
+    cost;
+
+    constructor(color, cost) {
+        this.color = color;
+        this.cost = cost;
+    }
+
+    newColor(color) {
+        this.color = color;
+    }
+
+    newCost(cost) {
+        this.cost = cost;
+    }
+
+}
--- Code/Shop/Shop.php
+++ Code/Shop/Shop.php
@@ -1,1 +1,14 @@
-<div class="shop">SHOP</div>
+<div class="shop">
+    <b>Shop</b>
+    <div class="items">
+        <button class="item">BG 1</button>
+        <button class="item">BG 2</button>
+        <button class="item">BG 3</button>
+        <button class="item">BG 4</button>
+        <button class="item">BG 5</button>
+        <button class="item">BG 6</button>
+        <button class="item">BG 7</button>
+        <button class="item">BG 8</button>
+        <button class="item">BG 9</button>
+    </div>
+</div>
